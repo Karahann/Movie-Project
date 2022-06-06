7dc54ea9e0453c4bb5613c132f5a8c7ba63264e4
Sefa Enes Karahan <71762829+Karahann@users.noreply.github.com>
2022-06-06 15:37:58 +0300

    Add files via upload

---
+++ Film-Project/project.js
@@ -0,0 +1,58 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+const cardBody = document.querySelectorAll(".card-body")[1];
+const clear = document.getElementById("clear-films");
+
+
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener("submit",addFilm);
+    document.addEventListener("DOMContentLoaded",function(){
+        let films = Storage.getFilmsFromStorage();
+        UI.loadAllFilms(films);
+    });
+    cardBody.addEventListener("click",deleteFilm);
+    clear.addEventListener("click",clearAllFilms);
+}
+
+function addFilm(e) {
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+    if (title === "" || director === "" || url ==="") {
+        UI.displayMessages("Tüm alanları doldurunuz...","danger");
+    }else{
+        const newFilm = new Film(title,director,url);
+
+        UI.addFilmToUI(newFilm);
+        Storage.addFilmToStorage(newFilm);
+        UI.displayMessages("Film başarıyla eklendi...","success");
+
+    }
+
+    UI.clearInputs(titleElement,urlElement,directorElement);
+    e.preventDefault();
+}
+
+function deleteFilm(e) {
+    
+    if (e.target.id === "delete-film") {
+        UI.deleteFilmFromUI(e.target);
+        Storage.deleteFilmFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+        UI.displayMessages("Film başarıyla silindi...","success")
+    }
+}
+
+function clearAllFilms() {
+    if (confirm("Emin misiniz ?")) {
+        UI.clearAllFilmsFromUI();
+        Storage.clearAllFilmsFromStorage();
+    }
+}
+
+
+
